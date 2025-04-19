14b07f99befac2e08943876788aab0f688e8c4d5
Skrubzeey <jednog000@pbs.edu.pl>
2025-04-19 12:03:19 +0200

    Adding few unit tests to evaluate the model

--- main.py
+++ main.py
@@ -1,16 +1,55 @@
-# This is a sample Python script.
+import joblib
+from sklearn.metrics import accuracy_score
+from sklearn.datasets import load_wine
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# załadowanie modelu oraz skalera
+model = joblib.load('wine_model.pkl')
+scaler = joblib.load('scaler.pkl')
+
+# ładowanie danych dla modelu
+data = load_wine(as_frame=True)
+X = data.data
+y = data.target
+
+# dokonanie predykcji modelu
+X_scaled = scaler.transform(X)
+preds = model.predict(X_scaled)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# stworzenie testów jednostkowych
+def test_predict_not_none():
+    """
+    Test 1: Sprawdza, czy otrzymujemy jakąkolwiek predykcję.
+    """
+    assert preds is not None
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def test_prediction_length():
+    """
+    Test 2: Sprawdza, czy długość listy predykcji jest większa
+    od 0 i czy odpowiada przewidywanej liczbie próbek testowych.
+    """
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    assert len(preds) > 0, "List cannot be empty"
+    assert len(preds) == len(y), f"Returned {len(preds)}, was supposed to get {len(y)}"
+
+
+def test_predictions_value_range():
+    """
+    Test 3: Sprawdza, czy wartości w predykcjach mieszczą się w
+    spodziewanym zakresie
+    """
+
+    valid_classes = set(y)
+    assert set(preds).intersection(y) == set(preds), "Predicted classes out of range"
+
+
+def test_model_accuracy():
+    """
+    Test 4: Sprawdza, czy model osiąga co najmniej 70% dokładności.
+    """
+    model_accuracy = accuracy_score(y, preds)
+    assert model_accuracy > 0.7, f"Model's accuracy is lower than threshold: {model_accuracy}"
+
+
+
